19fdc85ad7499f164c9ff3f1c7f154285654e27f
Merly Escalona <escalona10@gmail.com>
2020-08-17 15:15:47 -0700

    Fixing an issue with the length of the annotations

--- simpred.py
+++ simpred.py
@@ -34,28 +34,29 @@ def aa3_to_aa1(aaa):
 	
 def parseSNPEffFile(f):
 	for line in f:
-		if not line.startswith('#'):						# handle comments
-			if 'missense_variant' in line:					# fish out missense variants only
-				tmp = line.strip().split('\t')
-				scaffold = tmp[0]
-				coord = tmp[1]
-				ref = tmp[3]
-				var = tmp[4]
-				annotation = tmp[7]
-				#print(annotation)
-				pattern = r'([^\|]+_variant\|)'
-				new_string = re.sub(pattern, '[cuthere]\\1', annotation)
-				#print(new_string)
-				ann = new_string.split('[cuthere]')
-				ann = ann[1:]
-				#print(ann)
-				for item in ann:
-					vals = parseSNPEffAnn(item).values()
-					parsed = ''
-					for i in vals:
-						parsed = parsed + ' ' + i
-					if "MODIFIER" not in parsed:
-						print(scaffold, coord, ref, var, parsed.strip())
+		if not line.startswith('#'):					# handle comments
+			if  'missense_variant' in line:					# fish out missense variants only
+				# if  not 'scaffold_Mt' in line:					# fish out missense variants only
+					tmp = line.strip().split('\t')
+					scaffold = tmp[0]
+					coord = tmp[1]
+					ref = tmp[3]
+					var = tmp[4]
+					annotation = tmp[7]
+					#print(annotation)
+					pattern = r'([^\|]+_variant\|)'
+					new_string = re.sub(pattern, '[cuthere]\\1', annotation)
+					#print(line)
+					ann = new_string.split('[cuthere]')
+					ann = ann[1:]
+					for item in ann:
+						if not "MODIFIER" in item:
+							vals = parseSNPEffAnn(item).values()
+							parsed = ''
+							for i in vals:
+								parsed = parsed + ' ' + i
+							# if "MODIFIER" not in parsed:
+							print(scaffold, coord, ref, var, parsed.strip())
         
 def parseSNPEffAnn(ann):
 #['missense_variant|MODERATE|Sc9M7eS_1763_HRSCAF_2674_28679|gene07994|transcript|mRNA07994|protein_coding|4/14|c.3640G>A|p.Glu1214Lys|3640/5781|3640/5781|1214/1926||,T|', 'missense_variant|MODERATE|Sc9M7eS_1763_HRSCAF_2674_28679|gene07996|transcript|mRNA07996|protein_coding|4/13|c.3640G>A|p.Glu1214Lys|3640/5610|3640/5610|1214/1869||,T|', 'intron_variant|MODIFIER|Sc9M7eS_1763_HRSCAF_2674_28679|gene07995|transcript|mRNA07995|protein_coding|3/9|c.455-4448G>A||||||,T|', 'intron_variant|MODIFIER|Sc9M7eS_1763_HRSCAF_2674_28679|gene07997|transcript|mRNA07997|protein_coding|3/10|c.455-4448G>A||||||,T|', 'intron_variant|MODIFIER|Sc9M7eS_1763_HRSCAF_2674_28679|gene07998|transcript|mRNA07998|protein_coding|3/8|c.455-4448G>A||||||;AN=26;AC=7']
